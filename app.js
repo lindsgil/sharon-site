66a5c05cc80a19995d02a87cfde461a97edcdea1
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-03-02 18:05:44 -0800

    added second div

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
 'use strict';
 
-// var hiddenDiv = document.getElementById('text-1');
-// hiddenDiv.addEventListener('click', showDiv);
-//
-// function showDiv() {
-//   document.getElementById('about-cont').style.display = 'inline';
-// }
-
 function showDiv() {
   document.getElementById('about-cont').style.display = '';
 }
