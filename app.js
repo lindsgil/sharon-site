dd2c9c4629161546a35e23090a7e60e1e14b62b9
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-03-02 17:15:24 -0800

    made div re-appear

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 'use strict';
+
+// var hiddenDiv = document.getElementById('text-1');
+// hiddenDiv.addEventListener('click', showDiv);
+//
+// function showDiv() {
+//   document.getElementById('about-cont').style.display = 'inline';
+// }
+
+function showDiv() {
+  document.getElementById('about-cont').style.display = '';
+}
