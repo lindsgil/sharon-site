f17d0ac4b24117d385cf8e788e8517faebe1e7a5
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-03-17 23:42:35 -0700

    added nav bar

--- app.js
+++ app.js
@@ -2,4 +2,11 @@
 
 function showDiv() {
   document.getElementById('about-cont').style.display = '';
-}
+};
+
+$('.hamburger-menu-list').hide();
+$('.hamburger-button').click(function() {
+  $('.hamburger-menu-list').slideToggle('slow', function() {
+    $('.hamburger-menu-list').hide();
+  });
+});
